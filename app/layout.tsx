e7e4684092e269e000bc34e1991eee892ff63f08
Brandon Bryan <bshwag@gmail.com>
2023-05-02 13:40:55 -0400

    Removed unused authcontext value

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,20 +16,11 @@ interface ChildProps {
 
 export default function RootLayout({ children }: ChildProps) {
 	return (
-		<html
-			lang='en'
-			className={`${roboto.className} bg-gray-300/20`}
-			suppressHydrationWarning
-		>
+		<html lang='en' className={`${roboto.className} bg-gray-300/20`} suppressHydrationWarning>
 			<body className='flex min-h-screen min-w-[600px] flex-col items-center bg-gray-300/20 pt-28 dark:dark:bg-zinc-900'>
 				<Providers>
 					<div className='flex w-full justify-center px-7'>
-						<AuthProvider
-							user={initialState.user}
-							isLoggedIn={initialState.isLoggedIn}
-						>
-							{children}
-						</AuthProvider>
+						<AuthProvider user={initialState.user}>{children}</AuthProvider>
 					</div>
 				</Providers>
 			</body>
